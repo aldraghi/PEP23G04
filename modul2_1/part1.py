b6705b49a6a0e094c75fdb243fa6955ce28da3e7
aldraghi <alin.draghici@yahoo.com>
2023-08-24 19:04:36 +0300

    test

---
+++ modul2_1/part1.py
@@ -0,0 +1,15 @@
+name = 'Alin'
+print(name)
+
+age = 48
+print(age) # int
+
+age = 48.0
+print(age) # float
+
+age = 48 + 0j
+print(age) # complex
+
+
+result = type(age) # am creat o variabila
+print(result)
