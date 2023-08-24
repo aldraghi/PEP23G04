8b1c2375d3288f23bc4799bb4ed11242e08cda1d
aldraghi <alin.draghici@yahoo.com>
2023-08-24 21:54:13 +0300

    test

--- modul2_1/part1.py
+++ modul2_1/part1.py
@@ -1,15 +1,41 @@
-name = 'Alin'
-print(name)
+# name = 'Alin'
+# print(name)
+#
+# age = 48
+# print(age) # int
+#
+# age = 48.0
+# print(age) # float
+#
+# age = 48 + 0j
+# print(age) # complex
+#
+#
+# result = type(age) # am creat o variabila
+# print(result)
+#
+# print('Methods of string: ', dir(name))
+# #name.
+# #age.
+#
+# number1 = 5
+# number2 = number1
+# number2 = number1 + 1
+# print(number2)
+# print(number1)
+#
+# print('Return from print: ', print(37))
+# result1 = input('Input data here: ')
+# print(result1)
+# result2 = input('Input data here: ')
+# print(result2)
+# # print(type(result))
+# print('Result of add str: ',result1 + result2)
+# print('Result of add str: ', int(result1) + int(result2))
+#
+# #True = 3 -- nu poate fi variabila este cuvant cheie in Python
+#
+# print(True)
+# print(False)
+# print(Type(True))
 
-age = 48
-print(age) # int
-
-age = 48.0
-print(age) # float
-
-age = 48 + 0j
-print(age) # complex
-
-
-result = type(age) # am creat o variabila
-print(result)
