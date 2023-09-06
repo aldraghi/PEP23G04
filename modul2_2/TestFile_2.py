47533a484859f93402e0f195b7a98ca31d44c143
aldraghi <alin.draghici@yahoo.com>
2023-09-06 23:27:56 +0300

    test

---
+++ modul2_2/TestFile_2.py
@@ -0,0 +1,55 @@
+x = int (input())
+y = int (input())
+#
+x = x / y
+y = y / x
+print(y)
+
+
+# test9
+# x = 1
+# y = 2
+# z = x
+# x = y
+# y = z
+# print(x, y)
+#
+# test12
+#
+# print(1 / 1)
+
+# test15
+# x = input()
+# y = input()
+# print(x + y)
+
+# test17
+# x = int(input())
+# y = int(input())
+# #
+# # x = x % y
+# # x = x % y
+# # y = y % x
+# print(x + y)
+
+# test19
+
+# x = int(input())
+# y = int(input())
+#
+# x = x // y
+# y = y // x
+# print(y)
+
+# # test20
+# x= input()
+# y =int(input())
+# print(x * y)
+
+# test1
+# y = 2 + 3 * 5.
+# print(y)
+
+
+
+
